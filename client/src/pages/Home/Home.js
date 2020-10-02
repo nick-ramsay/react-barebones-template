e266cd19f3526fae0c6dc1b17b34ba99b0010454
nick-ramsay <NickRamsay1989@gmail.com>
2020-10-02 10:52:04 +1000

    Cleaned up formatting and code of this application

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,8 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { /*useState, useEffect*/ } from 'react';
 import logo from '../../../src/logo.svg';
 import "./style.css";
 
-const override = "display: block; margin: 0 auto; border-color: indigo;";
-
 const Home = () => {
-
     return (
         <div>
             <div className="App">
@@ -14,35 +11,9 @@ const Home = () => {
                     <p>Edit <code>src/App.js</code> and save to reload.</p>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
                 </header>
-                <form className="form-inline">
-                    <div className="row mt-3">
-                        <div className="col">
-                            <label for="staticEmail2" className="sr-only">Email</label>
-                            <textarea type="text" placeholder="Enter Test Message Here" />
-                        </div>
-                        <div className="col d-flex">
-                            <button type="button" className="btn btn-sm btn-custom mt-auto mb-auto">Save Message</button>
-                        </div>
-                    </div>
-                </form>
-                <div className="col-md-12 mt-5">
-                    <table class="table table-dark">
-                        <thead>
-                            <tr>
-                                <th scope="col">Messages</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>No Messages</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
             </div>
-        </div >
+        </div>
     )
-
 }
 
 export default Home;
